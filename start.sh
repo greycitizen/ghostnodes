64f7a793b56dbdd867be4c0e87e0afb3af6b7f26
Grey Citizen <greycitizen@proton.me>
2025-07-09 19:31:26 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ sudo systemctl stop cloud-init
 sudo systemctl disable cloud-init
 sudo systemctl disable --now unattended-upgrades
 #
-apt install net-tools vim htop lm-sensors nmap dos2unix -y
+apt install git net-tools vim htop lm-sensors nmap dos2unix -y
 #
 echo ""
 echo "##################################"
