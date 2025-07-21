f7a10cb471e43b94ab6ee5dee88e7d498840875b
Grey Citizen <greycitizen@proton.me>
2025-07-20 22:07:53 -0300

    Update script_s.sh

--- satoshi/script_s.sh
+++ satoshi/script_s.sh
@@ -4,6 +4,7 @@
 
 sudo mv /root/nodenation/satoshi/scripts/*.sh /home/pleb/
 sudo mv /root/nodenation/satoshi/script_s.sh /home/pleb/
+sudo mv /root/nodenation/satoshi/scripts/bitcoin.conf /home/pleb/
 
 # Verificação de segurança: bloqueia qualquer execução com privilégios
 if [ "$EUID" -eq 0 ] || [ -n "$SUDO_USER" ] || [ -n "$SUDO_UID" ] || pstree -ps $$ | grep -q 'sudo'; then
@@ -31,36 +32,5 @@ echo "######### Bitcoin Core Installation ###########"
 echo "###############################################"
 
 $HOME/installcore.sh </dev/tty
-
-echo "##### Aliases #####"
-echo '# Now ls be colors.
-alias ls="ls -la --color"
-# IP detailed
-alias ip="ip -c -br -a"
-# Otimized Update
-alias update="sudo apt update && sudo apt upgrade"
-# Checking Ports
-alias ports="sudo netstat -tulanp"
-# Files Size
-alias filesize="du -sh * | sort -h"
-# Last prompt
-alias gh="history|grep ''"
-# ?
-alias nf="neofetch"
-# cd ..
-alias ..="cd .."
-#
-alias c="clear"
-# VIM
-alias vi="vim"
-# Sudo
-alias root="sudo -i"
-#
-' >> $HOME/.bash_aliases
-
-echo ""
-echo "#########################"
-echo " Execute: source .bashrc "
-echo ""
 
 exit 0
