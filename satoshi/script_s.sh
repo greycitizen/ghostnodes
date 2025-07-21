79b3aba1cc343b314d992ebe8591d4a9d0d6fb3a
Grey Citizen <greycitizen@proton.me>
2025-07-20 21:33:02 -0300

    Update script_s.sh

--- satoshi/script_s.sh
+++ satoshi/script_s.sh
@@ -13,10 +13,12 @@ if [ "$EUID" -eq 0 ] || [ -n "$SUDO_USER" ] || [ -n "$SUDO_UID" ] || pstree -ps 
 	    echo "## Enter: exit ##"
 	    echo "## and execute this with your pleb user ##"
 	    echo "#########################################"
+    	    echo ""
+     	    echo " exit"
 	    echo ""
             echo " /home/pleb/./installcore.sh"
 	    echo ""
-            exit 1
+            exit 1     
         fi
 
 # ===============================================
