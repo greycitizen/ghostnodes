d376c018ff3d2870d427f3b9944158dfe82c3ede
Grey Citizen <greycitizen@proton.me>
2025-07-21 21:57:57 -0300

    Update start.sh
    Language change

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Start Ghost Nation Script v.01
+# Start Ghost Nation Script v.06
 #
-echo "Atualizando seu Servidor Ubuntu"
+echo "Updating your Ubuntu Server"
 sudo apt update && sudo apt upgrade -y
 #
 # Disabling unnecessary services
@@ -26,7 +26,6 @@ echo ""
 sudo find /root/nodenation/ -name "*.sh" -type f -print0 | xargs -0 chmod +x
 
 # Execute the Menu:
-echo ""
 echo "###########################################"
 echo "############ Make Your Ghost ##############"
 echo "###########################################"
@@ -46,27 +45,30 @@ echo ""
 pasta1="/root/nodenation/"
 
 case $escolha in
+
     1)
-            echo "
-Running Halfin Node..."
-echo "#################################
-     What is your equipment?
-#################################"
-echo "#################################
+        echo ""
+	echo "#################################"
+	echo "Running Halfin Node..."
+	echo ""
+	echo "#################################"
+    echo "#### What is your equipment? ####"
+	echo "#################################"
+	echo ""
 
-Ubuntu Server instalado em:
+	Ubuntu Server installed on:
 
-[1] RaspBerry Pi + Dongle Wifi
-[2] Raspberry Pi sem Dongle
-[3] Banana Pi Zero 
-[4] Orange Pi Zero 3 + Dongle
-#################################
-"
+	[1] RaspBerry Pi + Dongle Wifi
+	[2] Raspberry Pi no Dongle
+	[3] Banana Pi Zero 
+	[4] Orange Pi Zero 3 + Dongle
+	#################################
+	"
 	pasta2="/root/halfin/"
         read -p "Choose your config: " TIPO </dev/tty
         if [[ "$TIPO" == "1" ]]; then
 	newfolder="halfin"
-        #git clone https://github.com/greycitizen/ghostnodes.git $pasta1
+        
         $pasta2./alias.sh </dev/tty
         $pasta2./script_rasp.sh </dev/tty
         fi
@@ -76,20 +78,14 @@ Ubuntu Server instalado em:
 	
         if [[ "$TIPO" == "4" ]]; then
 	newfolder="halfin"
- 
- 	find $pasta2 -type f -name "*.sh" -print0 | xargs -0 dos2unix
-	echo " Changing permition to scripts: "
-	echo ""
-	find $pasta2 -name "*.sh" -type f -print0 | xargs -0 chmod +x
- 
+  
   	mv $pasta1'halfin' /root/
-        #git clone https://github.com/greycitizen/ghostnodes.git $pasta1
         $pasta2./alias.sh </dev/tty
         $pasta2./script_orange3.sh </dev/tty
         fi
 	
         if [[ "$TIPO" == "2" ]]; then
-        echo "Script em fase de testes"
+        echo "Script on test fase v0.4"
         exit 0
         fi
         ;;
@@ -97,42 +93,46 @@ Ubuntu Server instalado em:
 
 	echo ""
 	echo "##########################################"
-	echo "######## Installing Satoshi Node #########"
+	echo "######## Instalando Satoshi Node #########"
 	echo "##########################################"
 	echo ""
 
 	satoshi=$pasta1"satoshi/script_s.sh"
 
         if [ -f "$satoshi" ]; then
-            echo "Installing Satoshi Node..."
-            /bin/bash $satoshi </dev/tty
+            echo "Instalar o Satoshi Node..."
+            /bin/bash $satoshi
         else
-            echo "Erro: $satoshi não encontrado!"
+            echo "Erro: $satoshi not found!"
             exit 1
         fi
         ;;
     3)
-        if [ -f "/pleb/script_b.sh" ]; then
-            echo "Instalar o Satoshi Node..."
-            /bin/bash /pleb/script_b.sh </dev/tty
+        if [ -f "/nick/script_n.sh" ]; then
+            echo "Nick Node Installation..."
+            /bin/bash /nick/script_n.sh </dev/tty
         else
-            echo "Erro: /pleb/script_b.sh n�o encontrado!"
+            echo "######################################"
+	    echo "#### News coming soon for Nick... ####"
+            echo "######################################"
             exit 1
         fi
         ;;
     4)
-        if [ -f "/pleb/script_b.sh" ]; then
-            echo "Enganando o Craig Node..."
-            /bin/bash /pleb/script_b.sh </dev/tty
+        if [ -f "/craig/script_c.sh" ]; then
+            echo "Fooling Craig Node..."
+            /bin/bash /craig/script_c.sh </dev/tty
         else
-            echo "Erro: /pleb/script_b.sh n�o encontrado!"
+            echo "#######################################"
+	        echo "##### Erro: Fooling Craig Node... #####"
+            echo "#######################################"
             exit 1
         fi
         ;;
     *)
-        echo "Saindo sem executar nada."
+        echo "Nothing to do!"
 	echo ""
- 	echo "##### Caso queira voltar a instalação digite: sudo /root/$newfolder/menu.sh ######"
+ 	echo "##### If you want to go back to the installation, type: sudo /root/$newfolder/menu.sh ######"
  	echo ""
         exit 0
         ;;
