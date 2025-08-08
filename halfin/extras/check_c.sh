f4f9d3d7bb373834bddd7e8914925f0aedef62ab
Grey Citizen <greycitizen@proton.me>
2025-08-07 23:50:28 -0300

    Update check_c.sh
    Traceroute check for google.com

--- halfin/extras/check_c.sh
+++ halfin/extras/check_c.sh
@@ -4,7 +4,7 @@
 #
 echo ""
 echo "####################################"
-echo "## Configuração Ativa do Iptables ##"
+echo "## ConfiguraÃ§Ã£o Ativa do Iptables ##"
 echo "####################################"
 echo ""
 iptables -t nat -L -n -v
@@ -21,4 +21,7 @@ echo "####################################"
 echo "### Teste de Internet via Bridge ###"
 echo "####################################"
 echo ""
-ping 8.8.8.8 -c 3
+
+ping 8.8.8.8 -c 2
+
+traceroute google.com
