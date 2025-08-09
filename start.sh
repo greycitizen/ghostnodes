1e6998539bc92c9bf1ae080e1bb55d98cddcaf3b
Grey Citizen <greycitizen@proton.me>
2025-08-09 04:06:53 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 # Start Ghost Nation Script v.07
 # Colorize:
 # Cores para mensagens
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-CYAN='\033[0;36m'
-NC='\033[0m' # No Color
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+CYAN="\033[0;36m"
+NC="\033[0m" # No Color
 #
 echo "${YELLOW}Updating your Ubuntu Server${NC}"
 sudo apt update && sudo apt upgrade -y
