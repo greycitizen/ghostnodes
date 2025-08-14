18349a04c21e700a14bef2d684ffb66f33917f11
Grey Citizen <greycitizen@proton.me>
2025-08-14 18:34:08 -0300

    Update script_s.sh

--- satoshi/script_s.sh
+++ satoshi/script_s.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 #
 # Verificação de segurança: bloqueia qualquer execução com privilégios
-if [ "$EUID" -eq 0 ] || [ -n "$SUDO_USER" ] || [ -n "$SUDO_UID" ] || pstree -ps $$ | grep -q 'sudo'; then
-            echo ""
-	    echo "#########################################"
-	    echo "## Don't execute this script like root ##"
-	    echo "## Enter: exit ##"
-	    echo "## and execute this with your pleb user ##"
-	    echo "#########################################"
-    	    echo ""
-     	    echo " exit"
-	    echo ""
-            echo " /home/pleb/./installcore.sh"
-	    echo ""
-            exit 1     
-        fi
-;;
+#if [ "$EUID" -eq 0 ] || [ -n "$SUDO_USER" ] || [ -n "$SUDO_UID" ] || pstree -ps $$ | grep -q 'sudo'; then
+ #           echo ""
+#	    echo "#########################################"
+#	    echo "## Don't execute this script like root ##"
+#	    echo "## Enter: exit ##"
+#	    echo "## and execute this with your pleb user ##"
+#	    echo "#########################################"
+    	#    echo ""
+     #	    echo " exit"
+	#    echo ""
+ #           echo " /home/pleb/./installcore.sh"
+#	    echo ""
+#            exit 1     
+#        fi
+
 # ===============================================
 # A PARTIR DAQUI: EXECUÇÃO NORMAL (USUÁRIO COMUM)
 # ===============================================	
@@ -28,6 +28,7 @@ echo "###############################################"
 $HOME/installcore.sh </dev/tty
 
 exit 0
+
 
 
 
