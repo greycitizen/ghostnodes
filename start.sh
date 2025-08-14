be6cf1a2adef967ba8c805b12113fde896fdceed
Grey Citizen <greycitizen@proton.me>
2025-08-14 19:25:54 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -115,7 +115,7 @@ case $escolha in
 				sudo mv /root/nodenation/satoshi/script_s.sh /home/pleb/ </dev/tty
 				sudo mv /root/nodenation/satoshi/scripts/bitcoin.conf /home/pleb/ </dev/tty
 				sudo chown pleb:pleb /home/pleb/* </dev/tty
-	            /bin/bash /home/pleb/satoshi/script_s.sh </dev/tty
+	            /bin/bash /home/pleb/script_s.sh </dev/tty
 
 		else
             echo -e  "Erro: $satoshi not found!"
